cb0c9f737787a7a20cb7b419c76ca6b90fb9f470
Mark L. Chang <mark.chang@gmail.com>
2012-05-15 16:14:48 -0400

    handle csv import and table populate

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
-	$.get('data.csv', function(data) {
-		alert('Loaded data')
+	$.get('data.csv', function(data_csv) {
+		console.log('Retrieved data.csv')
+		var leaderboard = $.csv2Array(data_csv)
+		console.log(leaderboard)
+		for(var i = 1; i < leaderboard.length; i++) {
+			var name = leaderboard[i][0]
+			var points = leaderboard[i][1]
+			var achievements = leaderboard[i][2]
+
+			var html = "<tr><td>"+name+"</td><td>"+points+"</td><td>"+achievements+"</td></tr>"
+
+			$("#leaderboard-table").append(html)
+		}
 	})
-	$("#leaderboard-table").append("<tr><td>hi</td></tr>")
 })
